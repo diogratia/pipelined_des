92e43c538dd17f8ab2080625ebf2f4392ab5e62d
Dio Gratia <diogratia@gmail.com>
2018-07-09 10:22:04 +1200

    duplicate - remove

--- endian.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-#define IS_BIG_ENDIAN (!*(unsigned char *)&(uint16_t){1})
-
-int main () 
-{
-    printf("%s\n", IS_BIG_ENDIAN?"BIG ENDIAN":"LITTLE ENDIAN");
-    return (0);
-}
